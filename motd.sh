6e7de9406072dfe6cb61e20295cee535fd79be89
tchelospy <tchelospy@gmail.com>
2017-11-24 17:43:27 -0300

    Add files via upload

---
+++ motd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt update && apt upgrade -y
+apt install figlet -y
+apt install wget -y
+echo
+wget https://raw.githubusercontent.com/tchelospy/PHONE-TOOLS/master/motd
+echo
+cat motd > /data/data/com.termux/files/usr/etc/motd
+echo
+rm modt motd.sh
+echo
+figlet finish xD
+exit
